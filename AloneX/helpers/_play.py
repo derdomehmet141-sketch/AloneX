28364b5347014c7959611ec69034068860bd5703
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-01-29 00:09:53 +0300

    Update _play.py

--- AloneX/helpers/_play.py
+++ AloneX/helpers/_play.py
@@ -3,128 +3,122 @@
 # This file is part of AloneX
 
 
-from pathlib import Path
+import asyncio
 
-from pyrogram import filters, types
+from pyrogram import enums, errors, types
 
-from AloneX import anon, app, config, db, lang, queue, tg, yt
-from AloneX.helpers import buttons, utils
-from AloneX.helpers._play import checkUB
+from AloneX import app, config, db, logger, queue, yt
+from AloneX.helpers import utils
 
 
-def playlist_to_queue(chat_id: int, tracks: list) -> str:
-    text = "<blockquote expandable>"
-    for track in tracks:
-        pos = queue.add(chat_id, track)
-        text += f"<b>{pos}.</b> {track.title}\n"
-    text = text[:1948] + "</blockquote>"
-    return text
+def checkUB(play):
+    async def wrapper(_, m: types.Message):
+        if not m.from_user:
+            return await m.reply_text(m.lang["play_user_invalid"])
 
-@app.on_message(
-    filters.command(["oynat", "playforce", "voynat", "vplayforce"])
-    & filters.group
-    & ~app.bl_users
-)
-@lang.language()
-@checkUB
-async def play_hndlr(
-    _,
-    m: types.Message,
-    force: bool = False,
-    m3u8: bool = False,
-    video: bool = False,
-    url: str = None,
-) -> None:
-    sent = await m.reply_text(m.lang["play_searching"])
-    file = None
-    mention = m.from_user.mention
-    media = tg.get_media(m.reply_to_message) if m.reply_to_message else None
-    tracks = []
+        chat_id = m.chat.id
+        if m.chat.type != enums.ChatType.SUPERGROUP:
+            await m.reply_text(m.lang["play_chat_invalid"])
+            return await app.leave_chat(chat_id)
 
-    if url:
-        if "playlist" in url:
-            await sent.edit_text(m.lang["playlist_fetch"])
-            tracks = await yt.playlist(
-                config.PLAYLIST_LIMIT, mention, url, video
-            )
+        if not m.reply_to_message and (
+            len(m.command) < 2 or (len(m.command) == 2 and m.command[1] == "-f")
+        ):
+            return await m.reply_text(m.lang["play_usage"])
 
-            if not tracks:
-                return await sent.edit_text(m.lang["playlist_error"])
+        if len(queue.get_queue(chat_id)) >= config.QUEUE_LIMIT:
+            return await m.reply_text(m.lang["play_queue_full"].format(config.QUEUE_LIMIT))
 
-            file = tracks[0]
-            tracks.remove(file)
-            file.message_id = sent.id
-        else:
-            file = await yt.search(url, sent.id, video=video)
-
-        if not file:
-            return await sent.edit_text(
-                m.lang["play_not_found"].format(config.SUPPORT_CHAT)
-            )
-
-    elif len(m.command) >= 2:
-        query = " ".join(m.command[1:])
-        file = await yt.search(query, sent.id, video=video)
-        if not file:
-            return await sent.edit_text(
-                m.lang["play_not_found"].format(config.SUPPORT_CHAT)
-            )
-
-    elif media:
-        setattr(sent, "lang", m.lang)
-        file = await tg.download(m.reply_to_message, sent)
-
-    if not file:
-        return await sent.edit_text(m.lang["play_usage"])
-
-    if file.duration_sec > config.DURATION_LIMIT:
-        return await sent.edit_text(
-            m.lang["play_duration_limit"].format(config.DURATION_LIMIT // 60)
+        force = m.command[0].endswith("force") or (
+            len(m.command) > 1 and "-f" in m.command[1]
         )
+        video = m.command[0][0] == "v" and config.VIDEO_PLAY
+        url = utils.get_url(m)
+        m3u8 = url and not yt.valid(url)
 
-    if await db.is_logger():
-        await utils.play_log(m, file.title, file.duration)
+        play_mode = await db.get_play_mode(chat_id)
+        if play_mode or force:
+            adminlist = await db.get_admins(chat_id)
+            if (
+                m.from_user.id not in adminlist
+                and not await db.is_auth(chat_id, m.from_user.id)
+                and not m.from_user.id in app.sudoers
+            ):
+                return await m.reply_text(m.lang["play_admin"])
 
-    file.user = mention
-    if force:
-        queue.force_add(m.chat.id, file)
-    else:
-        position = queue.add(m.chat.id, file)
+        if chat_id not in db.active_calls:
+            client = await db.get_client(chat_id)
+            try:
+                member = await app.get_chat_member(chat_id, client.id)
+                if member.status in [
+                    enums.ChatMemberStatus.BANNED,
+                    enums.ChatMemberStatus.RESTRICTED,
+                ]:
+                    try:
+                        await app.unban_chat_member(
+                            chat_id=chat_id, user_id=client.id
+                        )
+                    except:
+                        return await m.reply_text(
+                            m.lang["play_banned"].format(
+                                app.name,
+                                client.id,
+                                client.mention,
+                                f"@{client.username}" if client.username else None,
+                            )
+                        )
+            except errors.ChatAdminRequired:
+                return await m.reply_text(m.lang["admin_required"])
+            except (errors.UserNotParticipant, errors.exceptions.bad_request_400.UserNotParticipant):
+                if m.chat.username:
+                    invite_link = m.chat.username
+                    try:
+                        await client.resolve_peer(invite_link)
+                    except:
+                        pass
+                else:
+                    try:
+                        invite_link = (await app.get_chat(chat_id)).invite_link
+                        if not invite_link:
+                            invite_link = await app.export_chat_invite_link(chat_id)
+                    except errors.ChatAdminRequired:
+                        return await m.reply_text(m.lang["admin_required"])
+                    except Exception as ex:
+                        return await m.reply_text(
+                            m.lang["play_invite_error"].format(type(ex).__name__)
+                        )
 
-        if position != 0 or await db.get_call(m.chat.id):
-            await sent.edit_text(
-                m.lang["play_queued"].format(
-                    position,
-                    file.url,
-                    file.title,
-                    file.duration,
-                    m.from_user.mention,
-                ),
-                reply_markup=buttons.play_queued(
-                    m.chat.id, file.id, m.lang["play_now"]
-                ),
-            )
-            if tracks:
-                added = playlist_to_queue(m.chat.id, tracks)
-                await app.send_message(
-                    chat_id=m.chat.id,
-                    text=m.lang["playlist_queued"].format(len(tracks)) + added,
-                )
-            return
+                umm = await m.reply_text(m.lang["play_invite"].format(app.name))
+                await asyncio.sleep(2)
+                try:
+                    await client.join_chat(invite_link)
+                except errors.UserAlreadyParticipant:
+                    pass
+                except errors.InviteRequestSent:
+                    await asyncio.sleep(2)
+                    try:
+                        await client.approve_chat_join_request(chat_id, client.id)
+                    except errors.HideRequesterMissing:
+                        pass
+                    except Exception as ex:
+                        return await umm.edit_text(
+                            m.lang["play_invite_error"].format(type(ex).__name__)
+                        )
+                except Exception as ex:
+                    logger.error(f"Error joining chat - {chat_id}: {ex}")
+                    return await umm.edit_text(
+                        m.lang["play_invite_error"].format(type(ex).__name__)
+                    )
 
-    if not file.file_path:
-        fname = f"downloads/{file.id}.{'mp4' if video else 'webm'}"
-        if Path(fname).exists():
-            file.file_path = fname
-        else:
-            await sent.edit_text(m.lang["play_downloading"])
-            file.file_path = await yt.download(file.id, video=video)
+                await umm.delete()
+                await client.resolve_peer(chat_id)
 
-    await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
-    if not tracks:
-        return
-    added = playlist_to_queue(m.chat.id, tracks)
-    await app.send_message(
-        chat_id=m.chat.id,
-        text=m.lang["playlist_queued"].format(len(tracks)) + added,
-    )
+        if await db.get_cmd_delete(chat_id):
+            try:
+                await m.delete()
+            except:
+                pass
+
+        return await play(_, m, force, m3u8, video, url)
+
+    return wrapper
